2460f28e750d8a03a133137f21924e7ff72a0d99
ClaraGrigolon <claragrigolon@gmail.com>
2025-05-22 12:26:48 -0300

    Add files via upload

--- js/megasena.js
+++ js/megasena.js
@@ -1,28 +1,28 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const generateBtn = document.getElementById ('generateBtn');
-    const numberElements = document.querySelectorAll ('.number');
+    const generateBtn = document.getElementById('generateBtn');
+    const numberElements = document.querySelectorAll('.number');
 
-    //gera numeros aleatorios ao carregar a pagina
+    //Gera números aleatórios ao carregar a página
     generateRandomNumbers();
 
     generateBtn.addEventListener('click', generateRandomNumbers);
 
     function generateRandomNumbers(){
-        //gera 6 numeros inteiros, unicos e aleatorios entre 1 e 60
+        //gera 6 números inteiros, únicos e aleatórios entre 1 e 60
         const numbers = [];
         while (numbers.length < 6){
-            const randomNum = Math.floor (Math.random() * 60) + 1;
-            if (!numbers.includes (randomNum)){
+            const randomNum = Math.floor(Math.random() * 60) + 1;
+            if (!numbers.includes(randomNum)){
                 numbers.push(randomNum);
             }
         }
 
-        //ordenar os numeros (como no sorteio)
-        numbers.sort(a, b => a - b);
+        //Ordenar os núm (como no sorteio)
+        numbers.sort((a, b) => a - b);
 
-        //atualizar os elementos com animação
+        //Atualizar os elementos com Animação
         numberElements.forEach((element, index) => {
-            //atualiza a classe da animação
+            //Atualiza a classe da animação
             element.classList.remove('animate');
 
             //tempo para remover a classe
@@ -30,7 +30,8 @@ document.addEventListener('DOMContentLoaded', function() {
                 element.textContent = numbers[index];
                 element.classList.add('animate');
             }, 50 * index);
-        }
+        });
 
     }
+
 });
